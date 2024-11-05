c942e5d68968d195cadf139f4ca7997428b6db8d
GentPodvorica <155079752+GentPodvorica@users.noreply.github.com>
2024-11-06 00:34:43 +0100

    Client-file 2
    Co-Authored-By: Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>

--- client/udp-client.js
+++ client/udp-client.js
@@ -25,3 +25,31 @@ const readLine = rl.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+readLine.question('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' +
+    'Shenoni kerkesen tuaj:\n\n' +
+    '    username; password; write <file> <message>\n' +
+                    '\t\t\tread <file>\n' +
+                    '\t\t\texecute <command>\n' +
+                    '\t\t\tkick <username>\n' +
+                    '\t\t\tprint\n' +
+                    '\t\t\tlogout\n' +
+                    '\t\t\t\'exit\' exit\n' +
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n', 
+(answer) => {
+if (answer.trim().toLowerCase() === 'exit') {
+readLine.close()
+return
+} else {
+client.send(answer, port, ip)
+}
+}
+)
+
+client.on('message', (msg, rinfo) => {
+console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n[serveri] => \n${msg}\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`)
+})
+
+readLine.on('line', (input) => {
+client.send(input, port, ip)
+})
